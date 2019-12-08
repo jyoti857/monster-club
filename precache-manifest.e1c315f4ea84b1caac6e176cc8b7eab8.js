a6eabd26d7a4d9cfdacbad17611aa001998e39ec
Jyoti RS <jyotiranjans@cnxsi.com>
2019-12-09 01:13:52 +0530

    Updates

---
+++ precache-manifest.e1c315f4ea84b1caac6e176cc8b7eab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9131ffbff34cd7dec519e8a4522111f1",
+    "url": "/monster-club/index.html"
+  },
+  {
+    "revision": "ab11f015ea3cfc4373c4",
+    "url": "/monster-club/static/css/main.7afead65.chunk.css"
+  },
+  {
+    "revision": "ae1c9343da64003f01db",
+    "url": "/monster-club/static/js/2.3e680ab6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-club/static/js/2.3e680ab6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab11f015ea3cfc4373c4",
+    "url": "/monster-club/static/js/main.e8e76b83.chunk.js"
+  },
+  {
+    "revision": "3db9d8ad0acb292aada2",
+    "url": "/monster-club/static/js/runtime-main.3abdb7d1.js"
+  }
+]);
